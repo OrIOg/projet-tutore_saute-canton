af8bea3ee852327b5e00e3c460bf2f37621a7ea3
Thibault Poisson <thibault_poisson@orange.fr>
2021-02-14 03:01:23 +0100

    Fixed db setup & queries

--- request_canton.py
+++ request_canton.py
@@ -9,18 +9,17 @@ import sqlite3
 
 endpoint_url = "https://query.wikidata.org/sparql"
 
-query1 = """SELECT ?commune_de_France ?commune_de_FranceLabel ?population ?border ?location ?region WHERE {
+query1 = """SELECT ?commune_de_France ?commune_de_FranceLabel ?population ?border ?location ?region ?insee WHERE {
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
   ?commune_de_France wdt:P31 wd:Q484170.
   ?commune_de_France wdt:P1082 ?population.
   ?commune_de_France wdt:P47 ?border.
   ?commune_de_France wdt:P625 ?location.
   ?region wdt:P31 wd:Q6465.
-}
-
-OFFSET %s
-LIMIT 20000
-"""
+  ?commune_de_France wdt:P374 ?insee.
+  FILTER(xsd:integer(?insee) >= %d).
+  FILTER(xsd:integer(?insee) <= %d).
+}"""
 
 query2 = """
 SELECT ?region ?regionLabel ?regionadj ?regionadjLabel WHERE {
@@ -39,11 +38,11 @@ def get_results(endpoint_url, query):
     sparql.setReturnFormat(JSON)
     return sparql.query().convert()
 
-def get_cities(endpoint_url, offset):
+def get_cities(endpoint_url, offset, nb=1000):
   user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
   # TODO adjust user agent; see https://w.wiki/CX6
   sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
-  sparql.setQuery(query1 % (offset))
+  sparql.setQuery(query1 % (offset, offset+nb))
   sparql.setReturnFormat(JSON)
   return sparql.query().convert()
 
@@ -54,7 +53,7 @@ def get_cities(endpoint_url, offset):
 results = get_results(endpoint_url, query1)
 print(type(results["results"]["bindings"][0]))
 for d in results["results"]["bindings"] :
-  for k, i in d.items() : 
+  for k, i in d.items() :
     print(k + "->", end="")
     print(i)
   break
@@ -62,3 +61,7 @@ for d in results["results"]["bindings"] :
 
 res = re.findall(r"[-+]?\d*\.\d+|\d+", 'Point(4.609722222 46.987222222)')
 print(" ".join(res))"""
+
+if __name__ == '__main__':
+  data = get_cities(endpoint_url, 14118, 1)
+  exit(data["results"]["bindings"][0]["commune_de_FranceLabel"]["value"] != "Caen")
--- sqldb.py
+++ sqldb.py
@@ -1,11 +1,18 @@
 import sqlite3
 import re
 import request_canton
+import argparse
+import sys
 
-def reset_table(name):
-    cursor.execute("DROP TABLE "+ name)
+# Informations disponible: https://www.insee.fr/fr/information/4316069
+START_COMMUNE = 1001
+MAX_COMMUNE = 95690
+BURST = 5
 
-#parse value of dic 
+def drop_table(db: sqlite3.Connection, name):
+    db.execute(f"DROP TABLE  IF EXISTS {name}")
+
+#parse value of dic
 def parse_uri(data):
     tokens = data["value"].split("/")
     return tokens[len(tokens) -1]
@@ -15,64 +22,90 @@ def get_point(point_str):
     return " ".join(res)
 
 #data is the big data list from the query
-def insert_into_regions(data):
+def insert_into_regions(db: sqlite3.Connection, data):
     print("begin insertion into regions")
     for d in data:
         region = parse_uri(d["region"])
         regionadj = parse_uri(d["regionadj"])
 
-        cursor.execute("INSERT INTO regions (code, name, neighbour_code, neighbour_name) VALUES (?,?,?,?)",
+        db.execute("INSERT INTO regions (code, name, neighbour_code, neighbour_name) VALUES (?,?,?,?)",
                 (region, d["regionLabel"]["value"], regionadj, d["regionadjLabel"]["value"]))
         canton.commit()
     print("insert into regions completed")
 
+def city_to_obj(data):
+    return(parse_uri(data["commune_de_France"]), data["commune_de_FranceLabel"]["value"], int(data["population"]["value"]),
+            parse_uri(data["border"]), get_point(data["location"]["value"]), parse_uri(data["region"]), int(data["insee"]["value"]), 0)
+
 #data is the big data list from the query
-def insert_into_cities(data):
-    print("begin insertion into cities")
-    for d in data:
-
-        cursor.execute("INSERT INTO cities (code, name, population, border, location, region, distance) VALUES (?,?,?,?,?,?,?)",
-            (parse_uri(d["commune_de_France"]), d["commune_de_FranceLabel"]["value"], int(d["population"]["value"]),
-            parse_uri(d["border"]), get_point(d["location"]["value"]), parse_uri(d["region"]), 0))
-        canton.commit()
-    print("insert into cities completed")
-
-def select_all_from(table):
-    cursor.execute("SELECT * FROM " + table)
-    return cursor.fetchall()
-
-canton = sqlite3.connect("canton.db")
-cursor = canton.cursor()
-
-reset_table("cities")
-
-cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='cities' ''')
-if cursor.fetchone()[0] == 1 :
-    print("table cities already created")
-else: 
-    print("creating table cities")
-    cursor.execute(''' CREATE TABLE cities
-        (code text, name text, population real, border text, location text, region text, distance INTEGER)''')
+def insert_into_cities(db: sqlite3.Connection, data):
+    nb_insertions = len(data)
+    nb_digits = len(str(nb_insertions))
+    for i, d in enumerate(data):
+        sys.stdout.write(f"\tInserting {i:0{nb_digits}}/{nb_insertions}   \r")
+        sys.stdout.flush()
+        city = city_to_obj(d)
+        db.execute("INSERT INTO cities (code, name, population, border, location, region, insee, distance) VALUES (?,?,?,?,?,?,?,?)", city)
     canton.commit()
-    print("table cities created")
+    print()
 
-r = request_canton.get_cities(request_canton.endpoint_url, 0)
-insert_into_cities(r["results"]["bindings"])
+def select_all_from(db, table):
+    db.execute("SELECT * FROM " + table)
+    return db.fetchall()
+
+def main(db: sqlite3.Connection):
+    db.execute(''' CREATE TABLE IF NOT EXISTS cities (code text, name text, population real, border text, location text, region text, insee INTEGER, distance INTEGER)''')
+    db.commit()
+
+    start = START_COMMUNE
+    query = db.execute('''SELECT MAX(insee) FROM cities''').fetchone()
+    if query and query[0]:
+        start = query[0]
+        print("Cities already exists.")
+        print(f"Nb cities  : {db.execute(''' SELECT count(DISTINCT code) FROM cities ''').fetchone()[0]}")
+        print(f"Nb entries : {db.execute(''' SELECT count(code) FROM cities ''').fetchone()[0]}")
 
 
+    while start < MAX_COMMUNE:
+        nb = min(MAX_COMMUNE-start, BURST)
+        print(f"Requesting: {start} => {start+nb}")
+        r = request_canton.get_cities(request_canton.endpoint_url, start, nb)
+        insert_into_cities(db, r["results"]["bindings"])
+        start = db.execute(''' SELECT MAX(insee) FROM cities''').fetchone()[0] + 1
+        print(f"Nb cities  : {db.execute(''' SELECT count(DISTINCT code) FROM cities ''').fetchone()[0]}")
+        print(f"Nb entries : {db.execute(''' SELECT count(code) FROM cities ''').fetchone()[0]}")
 
-reset_table("regions")
-cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='regions' ''')
-if cursor.fetchone()[0] == 1 :
-    print("table regions already created")
-else:
-    print("creating table regions")
-    cursor.execute(''' CREATE TABLE regions
-        (code text, name text, neighbour_code text, neighbour_name text)''')
-    canton.commit()
-    print("table regions created")
 
-r = request_canton.get_results(request_canton.endpoint_url, request_canton.query2)
-insert_into_regions(r["results"]["bindings"])
+    """
+    drop_table("regions")
+    cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='regions' ''')
+    if cursor.fetchone()[0] == 1 :
+        print("table regions already created")
+    else:
+        print("creating table regions")
+        cursor.execute(''' CREATE TABLE regions
+            (code text, name text, neighbour_code text, neighbour_name text)''')
+        db.commit()
+        print("table regions created")
 
-canton.close()
+    r = request_canton.get_results(request_canton.endpoint_url, request_canton.query2)
+    insert_into_regions(r["results"]["bindings"])
+
+    db.close()"""
+
+
+if __name__ == "__main__":
+    canton = sqlite3.connect("canton.db")
+
+    parser = argparse.ArgumentParser(description='Manage DBs for the project.')
+    actions = parser.add_argument_group(title="action", description="Action to perform with the DB.")
+    parser.add_argument('--reset-all', help='Drop & Reinit the DB.', action='store_true', default=False)
+
+    args = parser.parse_args()
+    if args.reset_all:
+        drop_table(canton, "cities")
+        drop_table(canton, "region")
+
+    main(canton)
+
+    canton.close()
